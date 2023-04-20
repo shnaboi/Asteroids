817e67679c069737707985a0bac2fc5c588fcd17
shnaboi <shane.karner@gmail.com>
2023-04-19 23:58:05 -0400

    Ship now moves, thrusting works

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const FPS = 30;
 const SHIP_SIZE = 30;
-const TURN_SPEED = 270;
+const SHIP_THRUST = 7 //acceleration of ship px/sec
+const TURN_SPEED = 270; //degrees per second
 
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
@@ -13,34 +14,39 @@ let ship = {
     y: canvas.height / 2,
     r: SHIP_SIZE / 2,
     a: 90/180 * Math.PI,
-    rot: 0
+    rot: 0,
+    thrust: false,
+    velocity: {
+        x: 0,
+        y: 0
+    }
 }
 
 setInterval(update, 1000 / FPS);
 
 function keyDown(/** @type {keyboardEvent} */ ev) {
     switch(ev.keyCode) {
-        case 37: // Left arrow
+        case 37: // Left arrow (rotate left)
             ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
             break;
-        case 38: // Up arrow
-
+        case 38: // Up arrow (thrust)
+            ship.thrust = true;
             break;
-        case 39: // right arrow
+        case 39: // right arrow (rotate right)
         ship.rot = -TURN_SPEED / 180 * Math.PI / FPS;
             break;
     }
 }
 
-function keyUp(ev) {
+function keyUp(/** @type {keyboardEvent} */ ev) {
     switch(ev.keyCode) {
-        case 37: // Left arrow
+        case 37: // Left arrow on keyUp
             ship.rot = 0;
             break;
-        case 38: // Up arrow
-
+        case 38: // Up arrow on keyUp
+            ship.thrust = false;
             break;
-        case 39: // right arrow
+        case 39: // right arrow on keyUp
         ship.rot = 0;
             break;
     }
@@ -50,6 +56,12 @@ function update() {
     //draw space
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //thrust ship
+    if (ship.thrust) {
+        ship.velocity.x += SHIP_THRUST * Math.cos(ship.a) / FPS;
+        ship.velocity.y -= SHIP_THRUST * Math.sin(ship.a) / FPS;
+    }
 
     //draw ship
     ctx.strokeStyle = 'white',
@@ -73,9 +85,12 @@ function update() {
     //rotate ship
     ship.a += ship.rot;
 
+    // move ship
+    ship.x += ship.velocity.x;
+    ship.y += ship.velocity.y;
+
     // center dot for testing
     ctx.fillStyle = "white"
     ctx.fillRect(ship.x -1, ship.y - 1, 2, 2)
 
-    //move ship
 }
